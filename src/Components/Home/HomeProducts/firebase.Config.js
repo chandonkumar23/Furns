0eab0b67e3f1b85f516f021f565ca530df3c47d0
chandonkumar23 <chandonkumar070@gmail.com>
2025-03-23 23:44:08 +0600

    added firebase

---
+++ src/Components/Home/HomeProducts/firebase.Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhc-Y5AJvUYPYmnbkCWI3RRcRtAbsIrS4",
+  authDomain: "fruns-ab254.firebaseapp.com",
+  projectId: "fruns-ab254",
+  storageBucket: "fruns-ab254.firebasestorage.app",
+  messagingSenderId: "149471483562",
+  appId: "1:149471483562:web:bb031f1b49ff857705e2d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
